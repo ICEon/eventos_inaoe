06f3f3b3aae18fd807614bf6d5046e00cf027c6e
ICEon <matrix_eon@yahoo.com>
2015-06-14 10:09:01 -0500

    Uno
    EVentos

---
+++ js/acciones.js
@@ -0,0 +1,36 @@
+// JavaScript Document
+
+// JavaScript Document
+$(document).ready(function(e) {	
+document.addEventListener("deviceready",function() {
+	$('#btneventos').on ('tap', function(){
+	  buscareventos('junio');
+	  $( "body" ).pagecontainer( "change", "#eventos", { transition: "slide" });
+	})
+	
+});
+
+function buscaeventos(mesb){
+
+	datos = "mes="+mesb;
+	$.ajax({
+		type: "POST",
+		url: "127.0.0.1/EventosINAOE/buscareventos.php",
+		data: datos
+	}).done(function(msg) {
+
+alert (msg);
+/*var Datos = JSON.parse (msg);
+
+$('#devic table td').eq(1).text(Datos['Nombre']);
+$('#devic table td').eq(3).text(Datos['ApellidoP']);
+$('#devic table td').eq(5).text(Datos['ApellidoM']);
+$('#devic table td').eq(7).text(Datos['Secundaria']);
+$('#devic table td').eq(9).text(Datos['POpcion']);
+$('#devic table td').eq(11).text(Datos['SOpcion']);
+$('#devic table td').eq(13).text(Datos['TOpcion']);
+$('#devic table td').eq(15).text(Datos['COpcion']);
+*/
+	});
+}
+});
