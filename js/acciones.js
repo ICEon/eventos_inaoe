fac7e8ec8eefe0f1296b14a4f197c2aa25159100
ICEon <matrix_eon@yahoo.com>
2015-06-14 17:14:52 -0500

    11

--- js/acciones.js
+++ js/acciones.js
@@ -4,23 +4,14 @@ function buscaeventos(){
 	
 	$.ajax({
 		
-		url: "http://192.168.1.67/EventosINAOE/buscareventos.php"
+		url: "http://192.168.1.67/EventosINAOE/buscareventos.php",
+		async: false
 	
 		
 	}).done(function(msg) {
 
 alert (msg);
-/*var Datos = JSON.parse (msg);
-
-$('#devic table td').eq(1).text(Datos['Nombre']);
-$('#devic table td').eq(3).text(Datos['ApellidoP']);
-$('#devic table td').eq(5).text(Datos['ApellidoM']);
-$('#devic table td').eq(7).text(Datos['Secundaria']);
-$('#devic table td').eq(9).text(Datos['POpcion']);
-$('#devic table td').eq(11).text(Datos['SOpcion']);
-$('#devic table td').eq(13).text(Datos['TOpcion']);
-$('#devic table td').eq(15).text(Datos['COpcion']);
-*/
+/*var Datos = JSON.parse (msg);*/
 	});
 }
 // JavaScript Document
@@ -32,8 +23,8 @@ document.addEventListener("deviceready",function() {
 	$('#btneventos').on ('tap', function(){
 	
 	  buscaeventos();
-	  alert ('fuera');
-//	  $( "body" ).pagecontainer( "change", "#eventos", { transition: "slide" });
+alert ('fuera');
+	  $( "body" ).pagecontainer( "change", "#eventos", { transition: "slide" });
 	})
 	
 });
