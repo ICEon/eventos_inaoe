44187cd6d706e19ef08049f2feaa5124492c38c4
ICEon <matrix_eon@yahoo.com>
2015-06-14 21:19:48 -0500

    12

--- js/acciones.js
+++ js/acciones.js
@@ -10,8 +10,28 @@ function buscaeventos(){
 		
 	}).done(function(msg) {
 
-alert (msg);
-/*var Datos = JSON.parse (msg);*/
+
+var DatosJSON = JSON.parse (msg);
+
+if (DatosJSON.datos == 1)
+ {
+$('#listado_eventos').empty();	 
+
+	for (var i = 0; i <DatosJSON.eventos.length  ; i++) {
+    var fechaevento = new Date(DatosJSON.eventos[i].FechaE);
+	alert (fechaevento.getUTCMonth());
+
+	
+/*
+$('#listado_eventos').append('<div style="width:100%; font-size:1em; font-weight:bold;" class="foto"><img src="http://192.168.1.30/practica12/'+DatosJSON.amigos[i].IdAmigo+'.jpg" class="famigo" id="'+DatosJSON.amigos[i].IdAmigo+'" style="vertical-align:middle;"><a href="#" class="detalle">'+DatosJSON.amigos[i].Nombre + ' '+ DatosJSON.amigos[i].ApPaterno + ' ' + DatosJSON.amigos[i].ApMaterno +'</a><hr></div>');
+*/
+}
+
+ }
+ else
+ {
+	 
+ }
 	});
 }
 // JavaScript Document
@@ -23,7 +43,8 @@ document.addEventListener("deviceready",function() {
 	$('#btneventos').on ('tap', function(){
 	
 	  buscaeventos();
-alert ('fuera');
+	  
+//	  $('listado_eventos')
 	  $( "body" ).pagecontainer( "change", "#eventos", { transition: "slide" });
 	})
 	
